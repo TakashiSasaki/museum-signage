c48205275493f8a85caf4dccaaf260bee674f98c
Takashi SASAKI <takashi316@gmail.com>
2026-02-24 16:08:45 +0900

    feat: add PWA support with manifest and service worker

---
+++ dist/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'nexus-signage-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './manifest.json',
+    './image.png',
+    './cover.jpg',
+    './space.jpg',
+    './bio.jpg',
+    './robot.jpg',
+    './energy.jpg'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
