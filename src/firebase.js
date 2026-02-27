b44512c342725797083947781f317569da15ec7c
takashi316 <takashi316@gmail.com>
2026-02-27 05:34:01 +0000

    fix: initialize Google Analytics

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
 // TODO: Add your web app's Firebase configuration
@@ -16,6 +17,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const messaging = getMessaging(app);
 
 export const requestForToken = () => {
